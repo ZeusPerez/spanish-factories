66fc1e92649c552fb0c9ccba39934350bdce0fb3
Zeus <zeusperezgarcia@gmail.com>
2020-04-04 08:42:36 +0200

    Bootstrap with create react app

---
+++ src/Application.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+import data from './data/data';
+import './Application.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiemV1c3BlcmV6IiwiYSI6ImNrOGEwNzBsNTBieG0zbHFtMnIxc251amcifQ.zMmbXZH1mFhktd2ZHdka-g';
+
+
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        lng: 	-3.703790,
+        lat: 40.416775,
+        zoom: 5.2
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/dark-v9',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+    // add markers to map
+    data.places.forEach(function(marker) {
+
+      // create a HTML element for each feature
+      var el = document.createElement('div');
+      el.className = 'marker';
+
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+        .addTo(map);
+    });
+
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <div ref={el => this.mapContainer = el} className="mapContainer" />
+      </div>
+    )
+  }
+}
+
+export default Application;
--- src/index.js
+++ src/index.js
@@ -1,54 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl';
-import data from './data/data';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiemV1c3BlcmV6IiwiYSI6ImNrOGEwNzBsNTBieG0zbHFtMnIxc251amcifQ.zMmbXZH1mFhktd2ZHdka-g';
+import Application from './Application';
+import * as serviceWorker from './serviceWorker';
 
 
-class Application extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        lng: 	-3.703790,
-        lat: 40.416775,
-        zoom: 5.2
-    };
-  }
+ReactDOM.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
 
-  componentDidMount() {
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/dark-v9',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
-    });
-    // add markers to map
-    data.places.forEach(function(marker) {
-
-      // create a HTML element for each feature
-      var el = document.createElement('div');
-      el.className = 'marker';
-
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-        .addTo(map);
-    });
-
-  }
-
-
-
-  render() {
-    return (
-      <div>
-        <div ref={el => this.mapContainer = el} className="mapContainer" />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<Application />, document.getElementById('app'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
